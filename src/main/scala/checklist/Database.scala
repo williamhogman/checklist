2c69ac62fc47d6ea73165870c1eca0118aee2985
Niklas Korz <niklaskorz@niklas-pc.(none)>
2012-11-24 22:03:20 +0100

    Implemented most of the prototype API

--- src/main/scala/checklist/Database.scala
+++ src/main/scala/checklist/Database.scala
@@ -3,12 +3,6 @@ package checklist
 import com.mongodb.casbah.MongoConnection
 
 object Database {
-  val mongoConn = MongoConnection()
-  val mongoDB = mongoConn("checklist_test")
-
-  def conn = mongoConn
-
-  def db = mongoDB
-
-  def apply(collection: String) = mongoDB(collection)
+  val connection = MongoConnection()
+  val db = connection("checklist_test")
 }
--- src/main/scala/checklist/controllers/PrototypeController.scala
+++ src/main/scala/checklist/controllers/PrototypeController.scala
@@ -1,39 +1,108 @@
 package checklist
 package controllers
 
+import checklist.Database.db
+
 import com.twitter.finatra.Controller
 import com.twitter.util.Future
 
-import checklist.models.UserModel
+import com.mongodb.util.JSON
+import com.mongodb.{BasicDBObject, BasicDBList}
+import com.mongodb.casbah.Imports.{DBObject, DBList}
+import com.mongodb.casbah.commons.{MongoDBObject, MongoDBList}
+
+import java.nio.charset.Charset
 
 class PrototypeController extends Controller {
   get("/users/:username/prototypes") { request =>
-    render.plain("Not implemented").toFuture
+    val username = request.routeParams("username")
+
+    var user = new MongoDBObject(
+      db("users").findOne(DBObject("name" -> username)).get)
+    var prototypes = user.as[MongoDBList]("prototypes")
+
+    render.json(prototypes).toFuture
   }
 
   post("/users/:username/prototypes") { request =>
-    render.plain("Not implemented").toFuture
+    val username = request.routeParams("username")
+    val prototype = JSON.parse(
+      request.content.toString(Charset.forName("UTF-8")))
+
+    var user = db("users").findOne(DBObject("name" -> username)).get
+    db("users").update(
+      user, DBObject("$push" -> DBObject("prototypes" -> prototype)))
+
+    render.plain("Success").toFuture
   }
 
   get("/users/:username/prototypes/:protoId") { request =>
     val username = request.routeParams("username")
     val protoId = request.routeParams("protoId").toInt
 
-    val user = UserModel(username)
-    val prototype = user.prototypes(protoId)
+    var user = new MongoDBObject(
+      db("users").findOne(DBObject("name" -> username)).get)
+    var prototypes = user.as[MongoDBList]("prototypes")
 
-    render.json(prototype.dbObject).toFuture
+    render.json(prototypes(protoId)).toFuture
   }
 
   post("/users/:username/prototypes/:protoId") { request =>
-    render.plain("Not implemented").toFuture
+    val username = request.routeParams("username")
+    val protoId = request.routeParams("protoId").toInt
+    val prototypeItem = JSON.parse(
+      request.content.toString(Charset.forName("UTF-8")))
+
+    val prototypeItemsKey = "prototypes." + protoId + ".items"
+
+    db("users").update(
+      DBObject("name" -> username),
+      DBObject("$push" -> DBObject(prototypeItemsKey -> prototypeItem)))
+
+    render.plain("Success").toFuture
   }
 
-  put("/users/:username/prototypes/:protoId") { request =>
-    render.plain("Not implemented").toFuture
-  }
+  /*put("/users/:username/prototypes/:protoId") { request =>
+    val username = request.routeParams("username")
+    val protoId = request.routeParams("protoId").toInt
+    val updateQuery = JSON.parse(
+      request.content.toString(Charset.forName("UTF-8"))).asInstanceOf[BasicDBObject]
+
+    db("users").update(DBObject("name" -> username), updateQuery)
+
+    render.plain("Success").toFuture
+  }*/
 
   delete("/users/:username/prototypes/:protoId") { request =>
-    render.plain("Not implemented").toFuture
+    val username = request.routeParams("username")
+    val protoId = request.routeParams("protoId").toInt
+
+    db("users").update(
+      DBObject("name" -> username),
+      DBObject("$unset" -> DBObject(
+        "prototypes." + protoId -> 1)))
+    db("users").update(
+      DBObject("name" -> username),
+      DBObject("$pull" -> DBObject("prototypes" -> null)))
+
+    render.plain("Success").toFuture
+  }
+
+  delete("/users/:username/prototypes/:protoId/:itemId") { request =>
+    val username = request.routeParams("username")
+    val protoId = request.routeParams("protoId").toInt
+    val itemId = request.routeParams("itemId").toInt
+
+    val prototypeItemsKey = "prototypes." + protoId + ".items"
+
+    db("users").update(
+      DBObject("name" -> username),
+      DBObject("$unset" -> DBObject(
+        prototypeItemsKey + "." + itemId -> 1)))
+    db("users").update(
+      DBObject("name" -> username),
+      DBObject("$pull" -> DBObject(prototypeItemsKey -> null)))
+
+    render.plain("Success").toFuture
   }
 }
