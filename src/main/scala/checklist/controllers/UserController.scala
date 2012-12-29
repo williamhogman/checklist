05f97a659c9f7fc167ffb4656dca24a7560c2583
William Hogman <me@whn.se>
2012-12-30 00:17:38 +0100

    added getting users by id to demonstrate DBClient type class magic

--- src/main/scala/checklist/controllers/UserController.scala
+++ src/main/scala/checklist/controllers/UserController.scala
@@ -4,6 +4,7 @@ package controllers
 import com.twitter.finatra.Controller
 import com.twitter.util.Future
 
+import checklist.DBClient._
 //import checklist.models.UserModel
 
 
@@ -23,5 +24,15 @@ class UserController extends Controller {
 
   delete("/users/:username") { request =>
     render.plain("Not implemented").toFuture
+  }
+
+  get("/userid/:userid") {
+    request =>
+    val uid = request.routeParams("userid")
+    val resp = DBClient.byId[User](uid) match {
+      case Some(x) => render.json(x)
+      case None => render.plain("not found").notFound
+    }
+    resp.toFuture
   }
 }
